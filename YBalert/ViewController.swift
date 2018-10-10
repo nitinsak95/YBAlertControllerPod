1ce84b0f5be46a2e150fd25777f6148b9323b9b2
affixusimac1 <viveking@outlook.com>
2018-10-10 14:47:33 +0530

    rr

--- YBalert/ViewController.swift
+++ YBalert/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import YBAlertController
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ac(_ sender: UIButton) {
+        let alertController = YBAlertController(title: nil, message: nil, style: .ActionSheet)
+        
+        alertController.addButton(icon: UIImage(named: "b"), title: "Tweet", action: {
+            self.feedbackHandler()
+        })
+        alertController.addButton(icon: UIImage(named: "b"), title: "Tweet", action: {
+            self.feedbackHandler()
+        })
+        
+         alertController.cancelButtonTitle = "Cancel"
+        
+        alertController.touchingOutsideDismiss = true
+        
 
+        
+       alertController.show()
+     
+    }
+    
+    @IBAction func al(_ sender: UIButton) {
+        let alertController = YBAlertController(title: nil, message: nil, style: .Alert)
+        
+        alertController.addButton(icon: UIImage(named: "b"), title: "Tweet", action: {
+            self.feedbackHandler()
+        })
+        alertController.addButton(icon: UIImage(named: "b"), title: "Tweet", action: {
+            self.feedbackHandler()
+        })
+        
+        alertController.cancelButtonTitle = "Cancel"
+        
+        alertController.show()
+        
+    }
+    
+    func tap() {
+        print("tap")
+    }
+    
+    func feedbackHandler(){
+        print("fee")
+    }
 }
 
